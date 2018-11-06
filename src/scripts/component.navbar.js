78939eb1ef88330c058af6f594c91f5f8623b349
Callum Flack <callumflack@gmail.com>
2018-11-06 17:44:35 +1000

    feature: fixed nav on scroll

---
+++ src/scripts/component.navbar.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  // Initial scroll position
+  var scrollState = 0;
+
+  // Store navbar classes
+  var navClasses = document.getElementById('js-navbar').classList;
+
+  // returns current scroll position
+  var scrollTop = function () {
+    return window.scrollY;
+  };
+
+  // Primary scroll event function
+  var scrollDetect = function (home, down, up) {
+    // Current scroll position
+    var currentScroll = scrollTop();
+    if (scrollTop() === 0) {
+      home();
+    } else if (currentScroll > scrollState) {
+      down();
+    } else {
+      up();
+    }
+    // Set previous scroll position
+    scrollState = scrollTop();
+  };
+
+  function homeAction() {
+    console.log("home");
+  }
+
+  function downAction() {
+    navClasses.remove('open');
+    navClasses.add('collapse');
+  }
+
+  function upAction() {
+    navClasses.remove('collapse');
+    navClasses.add('open');
+  }
+
+  window.addEventListener("scroll", function () {
+    scrollDetect(homeAction, downAction, upAction);
+  });
+
+});
